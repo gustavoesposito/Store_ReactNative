343f6d1a939d17f319d115015837a5cb52069364
Gustavo Esposito <105567405+gustavoesposito@users.noreply.github.com>
2022-09-28 19:57:38 -0300

    attt

--- store/App.js
+++ store/App.js
@@ -2,8 +2,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { AppLoading } from 'expo';
-import { useFonts, Anton_400Regular} from '@expo-google-fonts/anton';
 
+import { useFonts, Anton_400Regular } from '@expo-google-fonts/anton';
 import Routes from './src/router';
 
 export default function App() {
